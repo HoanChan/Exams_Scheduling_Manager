846649b8d3085e13c768c89c81f21b158c54fc72
HoanChan <Le.Hoan.Chan@gmail.com>
2012-09-25 01:29:48 +0700

    Cài đặt cơ bản thuật toán tô màu đồ thị chạy trên Console
    Dùng thuật toán Recursive-Largest-First của F. T. Leighton

---
+++ Graph_Coloring/Graph_Coloring/Graph_Coloring/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Graph_Coloring
+{
+    class Program
+    {
+        static int[,] ReadAdjacencyMatrix(string DataFilePath)
+        {
+            int[,] AdjacencyMatrix = null;
+            string[] Data = File.ReadAllLines(DataFilePath);
+            int AdjacencyMatrixSize = Data.Length;
+            string[] Split = null;
+            AdjacencyMatrix = new int[AdjacencyMatrixSize, AdjacencyMatrixSize];
+            for (int i = 0; i < AdjacencyMatrixSize; i++)
+            {
+                Split = Data[i].Split(new char[] { ' ' });
+                for (int j = 0; j < Split.Length; j++)
+                    AdjacencyMatrix[i, j] = Convert.ToInt32(Split[j]);
+            }
+            return AdjacencyMatrix;
+        }
+
+        static void GetInput()
+        {
+            ReadAdjacencyMatrix(System.IO.Path.GetDirectoryName( System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\input.txt");
+        }
+
+
+        static void Main(string[] args)
+        {
+            IEnumerable<int> Color = GraphColoringAlgorithm.Run(ReadAdjacencyMatrix(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\input.txt"));
+            int i=0;
+            foreach (int C in Color)
+            {
+                i++;
+                Console.Write("Vertex ");
+                Console.Write(i);
+                Console.Write(" is colored ");
+                Console.Write(C);
+                Console.Write("\n");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
